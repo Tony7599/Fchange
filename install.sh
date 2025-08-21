b20ba1db9f128ec6fb03d47333053321a9f8bba8
Tony7599 <97866060+Tony7599@users.noreply.github.com>
2025-08-21 19:38:19 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~ 
+# 简洁版安装脚本
+echo "🔧 下载 Fchange..."
+pkg install wget -y && \
+wget -q https://raw.githubusercontent.com/tony7599/Fchange/main/main1 -O main1 && \
+chmod +x main1
+echo "使用sh main1运行"
