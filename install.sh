a4f0438bcc5f213b6c6975d9422a5e04f3c14abc
Tony7599 <97866060+Tony7599@users.noreply.github.com>
2025-08-21 19:55:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ echo "🔧 下载 Fchange..."
 pkg install wget -y && \
 wget -q https://raw.githubusercontent.com/tony7599/Fchange/main/main1 -O main1 && \
 chmod +x main1
-cp main1 /data/data/com.android.shell
-echo "本软件要shell权限运行 命令“sh /data/data/com.android.shell/main1”"
+echo "本软件要shell权限运行 命令“cp /data/data/com.termux/files/home/main1 && cp /data/data/com.termux/files/home/main1 /data/data/com.android.shell/”"
